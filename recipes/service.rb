b86e19fd8e5e46ffd6a468751201a55e8441ab24
Anton Lundin <anton@dohi.se>
2014-05-14 16:23:00 +0200

    Remove really annoying sleep

--- recipes/service.rb
+++ recipes/service.rb
@@ -7,9 +7,8 @@ end
 node[:deploy].each do |application, deploy|
   
   # Overwrite the unicorn restart command declared elsewhere
-  # Apologies for the `sleep`, but monit errors with "Other action already in progress" on some boots.
   execute "restart Rails app #{application}" do
-    command "sleep 300 && #{node[:delayed_job][application][:restart_command]}"
+    command "#{node[:delayed_job][application][:restart_command]}"
     action :nothing
   end
   
